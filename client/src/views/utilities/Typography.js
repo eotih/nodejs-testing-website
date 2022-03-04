c59cf77dd66cf19c296f0e3d5a4c0b06c7f88bd8
EOTIH <eotihvn@gmail.com>
2022-03-04 20:35:05 +0700

    POST dữ liệu thành công

--- client/src/views/utilities/Typography.js
+++ client/src/views/utilities/Typography.js
@@ -1,62 +1,86 @@
-import { Box, Button, Select, InputLabel, MenuItem, FormControl, Grid, TextField } from '@mui/material';
+import { Box, Button, Select, InputLabel, MenuItem, Stack, TextField } from '@mui/material';
 import { useState } from 'react';
+import { useFormik, Form, FormikProvider } from 'formik';
 
 import AnimateButton from 'ui-component/extended/AnimateButton';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
 import SecondaryAction from 'ui-component/cards/CardSecondaryAction';
-import { gridSpacing } from 'store/constant';
 
 // ==============================|| TYPOGRAPHY ||============================== //
-
+const INITIAL_VALUE = {
+    website: 'https://maiamtruyentin.com/he-thong/dang-nhap',
+    userId: '#UserName',
+    passId: '#Password',
+    buttonId: 'btn btn-primary',
+    kindOfTest: 2
+};
 const Typography = () => {
-    const [age, setAge] = useState('');
+    const [kindOfTest, setKindOfTest] = useState('');
 
+    const formik = useFormik({
+        initialValues: {
+            website: '',
+            userId: '',
+            passId: '',
+            buttonId: '',
+            kindOfTest
+        },
+        onSubmit: async () => {
+            fetch(`http://localhost:5000/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(INITIAL_VALUE)
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    });
+    const { handleSubmit, getFieldProps } = formik;
     const handleChange = (event) => {
-        setAge(event.target.value);
+        setKindOfTest(event.target.value);
+        formik.setFieldValue('kindOfTest', event.target.value);
     };
     return (
         <MainCard title="Test Login" secondary={<SecondaryAction link="https://next.material-ui.com/system/typography/" />}>
-            <Grid container spacing={gridSpacing}>
-                <Grid item xs={12} sm={12}>
-                    <Grid container direction="row" spacing={1}>
-                        <FormControl sx={{ mb: 2 }} fullWidth>
-                            <TextField fullWidth id="outlined-basic" label="Website" variant="outlined" />
-                        </FormControl>
-                        <FormControl sx={{ mb: 2 }} fullWidth>
-                            <TextField fullWidth id="outlined-basic" label="Website" variant="outlined" />
-                        </FormControl>
-                        <FormControl sx={{ mb: 2 }} fullWidth>
-                            <TextField fullWidth id="outlined-basic" label="Website" variant="outlined" />
-                        </FormControl>
-                        <FormControl sx={{ mb: 2 }} fullWidth>
-                            <TextField fullWidth id="outlined-basic" label="Website" variant="outlined" />
-                        </FormControl>
-                        <FormControl fullWidth>
-                            <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                            <Select
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={age}
-                                label="Age"
-                                onChange={handleChange}
-                            >
-                                <MenuItem value={10}>Ten</MenuItem>
-                                <MenuItem value={20}>Twenty</MenuItem>
-                                <MenuItem value={30}>Thirty</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Grid>
+            <FormikProvider value={formik}>
+                <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+                    <Stack spacing={2}>
+                        <TextField id="outlined-basic" label="Website" {...getFieldProps('website')} variant="outlined" />
+                        <TextField id="outlined-basic" label="User ID" {...getFieldProps('userId')} variant="outlined" />
+                        <TextField id="outlined-basic" label="Pass ID" {...getFieldProps('passId')} variant="outlined" />
+                        <TextField id="outlined-basic" label="Button ID" {...getFieldProps('buttonId')} variant="outlined" />
+
+                        <InputLabel id="demo-simple-select-label">Kind of test</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="kindOfTest"
+                            value={kindOfTest}
+                            label="Kind of test"
+                            name="kindOfTest"
+                            onChange={handleChange}
+                        >
+                            <MenuItem value={2}>Default</MenuItem>
+                            <MenuItem value={1}>SQL Injection</MenuItem>
+                        </Select>
+                    </Stack>
                     <Box sx={{ mt: 2 }}>
                         <AnimateButton>
-                            <Button disableElevation fullWidth size="large" type="submit" variant="contained" color="secondary">
-                                Sign in
+                            <Button fullWidth disableElevation size="large" type="submit" variant="contained" color="secondary">
+                                Test
                             </Button>
                         </AnimateButton>
                     </Box>
-                </Grid>
-            </Grid>
+                </Form>
+            </FormikProvider>
         </MainCard>
     );
 };
